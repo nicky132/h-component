793a51a5ef30efcb5f8766882e945270d18383d6
nicky132 <hhq87776153@hotmail.com>
2022-12-05 01:00:37 +0800

    monorepo

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -1,25 +1,25 @@
+// .fatherrc.ts
+
 import { readdirSync } from 'fs';
 import { join } from 'path';
-
 // utils must build before core
 // runtime must build before renderer-react
 // components dependencies order: form -> table -> list
-
-const headPkgs: string[] = ['button', 'tag']; // 添加button和tag
+const headPkgs: string[] = ['button']; // 这里改为自己需要打包的包名
 const tailPkgs = readdirSync(join(__dirname, 'packages')).filter(
   (pkg) => pkg.charAt(0) !== '.' && !headPkgs.includes(pkg),
 );
 
 const type = process.env.BUILD_TYPE;
-
 let config = {};
 
 if (type === 'lib') {
   config = {
-    cjs: { type: 'babel', lazy: true },
+    cjs: {},
     esm: false,
-    runtimeHelpers: true,
-    pkgs: [...headPkgs, ...tailPkgs],
+    // 这里改为false, 不然npm run build会报@babel/runtime dependency is required to use runtimeHelpers 的错误
+    // runtimeHelpers: false, 
+    // pkgs: [...headPkgs, ...tailPkgs],
     extraBabelPlugins: [
       ['babel-plugin-import', { libraryName: 'antd', libraryDirectory: 'es', style: true }, 'antd'],
     ],
@@ -29,15 +29,21 @@ if (type === 'lib') {
 if (type === 'es') {
   config = {
     cjs: false,
-    esm: {
-      type: 'babel',
-    },
-    runtimeHelpers: true,
-    pkgs: [...headPkgs, ...tailPkgs],
-    extraBabelPlugins: [
-      [require('./scripts/replaceLib')],
-      ['babel-plugin-import', { libraryName: 'antd', libraryDirectory: 'es', style: true }, 'antd'],
-    ],
+    esm: {},
+    // 这里改为false, 不然npm run build会报@babel/runtime dependency is required to use runtimeHelpers 的错误
+    // runtimeHelpers: false,
+    // pkgs: [...headPkgs, ...tailPkgs],
+    // extraBabelPlugins: [
+    //   [require('./scripts/replaceLib')],
+    //   ['babel-plugin-import', { libraryName: 'antd', libraryDirectory: 'es', style: true }, 'antd'],
+    // ],
+    // cssModules: true,
+    // extractCSS: false,
+    // lessInBabelMode: true,
+    // entry: ['packages/button/src/index.tsx', 'packages/tag/src/index.tsx'],
+    autoprefixer: {
+      browsers: ['ie>9', 'Safari >= 6'],
+    }
   };
 }
 
---
+++ tests/setupTests.js
@@ -0,0 +1,157 @@
+// import Adapter from '@cfaester/enzyme-adapter-react-18';
+import Enzyme from 'enzyme';
+import 'jest-canvas-mock';
+// import jest from 'jest';
+import { enableFetchMocks } from 'jest-fetch-mock';
+import MockDate from 'mockdate';
+import React from 'react';
+import tableData from './table/mock.data.json';
+
+import { defaultConfig } from 'antd/es/theme/index';
+
+defaultConfig.hashed = false;
+
+jest.mock('antd', () => {
+  const antd = jest.requireActual('antd');
+  antd.theme.defaultConfig.hashed = false;
+
+  return antd;
+});
+
+process.env.TZ = 'UTC';
+
+global.React = React;
+
+jest.mock('react', () => ({
+  ...jest.requireActual('react'),
+  useLayoutEffect: jest.requireActual('react').useEffect,
+}));
+
+jest.setTimeout(60000);
+
+const Adapter = process.env.REACT === '16'
+    ? require('enzyme-adapter-react-16')
+    : require('@wojtekmaj/enzyme-adapter-react-17');
+
+Enzyme.configure({ adapter: new Adapter() });
+
+/* eslint-disable global-require */
+if (typeof window !== 'undefined') {
+  // ref: https://github.com/ant-design/ant-design/issues/18774
+  if (!window.matchMedia) {
+    Object.defineProperty(global.window, 'matchMedia', {
+      writable: true,
+      configurable: true,
+      value: jest.fn(() => ({
+        matches: false,
+        addListener: jest.fn(),
+        removeListener: jest.fn(),
+      })),
+    });
+  }
+  if (!window.matchMedia) {
+    Object.defineProperty(global.window, 'matchMedia', {
+      writable: true,
+      configurable: true,
+      value: jest.fn((query) => ({
+        matches: query.includes('max-width'),
+        addListener: jest.fn(),
+        removeListener: jest.fn(),
+      })),
+    });
+  }
+}
+
+enableFetchMocks();
+
+Object.defineProperty(window, 'open', {
+  value: jest.fn,
+});
+
+const crypto = require('crypto');
+
+Object.defineProperty(global.self, 'crypto', {
+  value: {
+    getRandomValues: (arr) => crypto.randomBytes(arr.length),
+  },
+});
+
+global.requestAnimationFrame =
+  global.requestAnimationFrame ||
+  function requestAnimationFrame(cb) {
+    return setTimeout(cb, 0);
+  };
+
+global.cancelAnimationFrame =
+  global.cancelAnimationFrame ||
+  function cancelAnimationFrame() {
+    return null;
+  };
+
+// browserMocks.js
+export const localStorageMock = (() => {
+  let store = {
+    umi_locale: 'zh-CN',
+  };
+
+  return {
+    getItem(key) {
+      return store[key] || null;
+    },
+    setItem(key, value) {
+      store[key] = value.toString();
+    },
+    removeItem(key) {
+      store[key] = null;
+    },
+    clear() {
+      store = {};
+    },
+  };
+})();
+
+Object.defineProperty(window, 'localStorage', {
+  value: localStorageMock,
+  writable: true,
+});
+
+Object.defineProperty(window, 'cancelAnimationFrame', {
+  value: () => null,
+});
+
+// 2016-11-22 15:22:44
+MockDate.set(1479828164000);
+
+Math.random = () => 0.8404419276253765;
+
+fetch.mockResponse(async () => {
+  return { body: JSON.stringify(tableData) };
+});
+
+// @ts-ignore-next-line
+global.Worker = class {
+  constructor(stringUrl) {
+    // @ts-ignore-next-line
+    this.url = stringUrl;
+    // @ts-ignore-next-line
+    this.onmessage = () => {};
+  }
+
+  postMessage(msg) {
+    // @ts-ignore-next-line
+    this.onmessage(msg);
+  }
+};
+
+const errorLog = console.error;
+Object.defineProperty(global.window.console, 'error', {
+  writable: true,
+  configurable: true,
+  value: (...rest) => {
+    const logStr = rest.join('');
+    if (logStr.includes('Warning: An update to %s inside a test was not wrapped in act(...)')) {
+      return;
+    }
+    errorLog(...rest);
+  },
+});
